ab2fd19a8703458aebc7e951634c76ca4602d49a
Nisha0093 <125026651+Nisha0093@users.noreply.github.com>
2023-02-10 17:52:25 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,13 @@
+
+
+function validation(){
+
+    var username=document.getElementById("username");
+    var password=document.getElementById("password");
+
+    if((username.trim==" ")||(password.trim==" "))
+    {
+        alert("Please enter username");
+        
+    }
+}
