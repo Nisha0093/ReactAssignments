dcd4525c27abc8de7930db4d39743692b5936fcb
Nisha0093 <125026651+Nisha0093@users.noreply.github.com>
2023-02-17 20:02:09 +0530

    Add files via upload

---
+++ Assignment-2/Assignment3.js
@@ -0,0 +1,3 @@
+import * as utils from "./calculation.js"
+
+console.log("Simple interest is:",utils.interest(10000,2,12))
---
+++ Assignment-2/calculation.js
@@ -0,0 +1,8 @@
+
+
+export function interest(amount,interstrate,duration){
+
+    let inter=(amount*interstrate*duration)/100;
+    return inter;
+}
+
